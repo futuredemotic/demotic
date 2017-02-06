a2f6da026c504bdf7784c795518dc1105f5a6f41
futuredemotic <demoticsp@gmail.com>
2017-02-06 10:27:27 +0100

    Update domoticz_backup.sh

--- scripts_domoticz/domoticz_backup.sh
+++ scripts_domoticz/domoticz_backup.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
- 
-    ## LOCAL/FTP/SCP/MAIL PARAMETERS
-    SERVER="ft.sparchitettura.org"         # IP of Network disk, used for: ftp mail scp
-    USERNAME="4916475@aruba.it"         # FTP username of Network disk used for: ftp mail scp
-    PASSWORD="LUNAPORTOS77"               # FTP password of Network disk used for: ftp mail scp
-    DESTDIR="/www.sparchitettura.org/demotic/backup"      # used for: local
-    DOMO_IP="192.168.1.100"      # Domoticz IP used for all
-    DOMO_PORT="8080"         # Domoticz port used for all
-    ## END OF USER CONFIGURABLE PARAMETERS
- 
+    # LOCAL/FTP/SCP/MAIL PARAMETERS
+    SERVER="ftp.sparchitettura.org"   # IP of Network disk, used for ftp
+    USERNAME="4916475@aruba.it"         # FTP username of Network disk used for ftp
+    PASSWORD="LUNAPORTOS77"         # FTP password of Network disk used for ftp
+    DESTDIR="/opt/backup"   # used for temorarily storage
+    DOMO_IP="192.168.1.100"  # Domoticz IP 
+    DOMO_PORT="8080"        # Domoticz port 
+    LOGFILE="domoticz.txt"
+
+    ### END OF USER CONFIGURABLE PARAMETERS
     TIMESTAMP=`/bin/date +%Y%m%d%H%M%S`
-    BACKUPFILEDIR="domoticz_x_xxx_$TIMESTAMP.tar.gz" # Change the xxx to yours
- 
-    ### Create backup and ZIP it
-    tar -zcvf /tmp/$BACKUPFILEDIR /home/pi/domoticz/xxxx/   # Change the xxx to yours    # Or try /home/pi/domoticz/
- 
+    BACKUPFILE="domoticz_$TIMESTAMP.db" # backups will be named "domoticz_YYYYMMDDHHMMSS.db.gz"
+    BACKUPFILEGZ="$BACKUPFILE".gz
+    
+    ### Stop Domoticz, create backup, ZIP it and start Domoticz again
+    service domoticz.sh stop
+    /usr/bin/curl -s http://$DOMO_IP:$DOMO_PORT/backupdatabase.php > /tmp/$BACKUPFILE
+    /bin/cp /tmp/$LOGFILE /tmp/"$LOGFILE_$TIMESTAMP"
+    service domoticz.sh start
+    gzip -9 /tmp/$BACKUPFILE
+    
+
     ### Send to Network disk through FTP
-    curl -s --disable-epsv -v -T"/tmp/$BACKUPFILEDIR" -u"$USERNAME:$PASSWORD" "ftp://$SERVER/media/hdd/Domoticz_backup/"  # Change the ftp to yours !!!
- 
+    curl -s --disable-epsv -v -T"/tmp/$BACKUPFILEGZ" -u"$USERNAME:$PASSWORD" "ftp://$SERVER/www.sparchitettura.org/demotic/backup/"
+    curl -s --disable-epsv -v -T"/tmp/$LOGFILE_$TIMESTAMP" -u"$USERNAME:$PASSWORD" "ftp://$SERVER/www.sparchitettura.org/demotic/backup/"
     ### Remove temp backup file
-    /bin/rm /tmp/$BACKUPFILEDIR
- 
+    /bin/rm /tmp/$BACKUPFILEGZ
+    /bin/rm /tmp/$LOGFILE_$TIMESTAMP
     ### Done!
